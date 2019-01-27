55e409b28ccb7f0d0e70e479063747e0cb98e180
Sweta Ghimire <sghimi3@lsu.edu>
2019-01-26 23:49:40 -0600

    Merged both maps into one page

--- maps.py
+++ maps.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
 from flaskext.mysql import MySQL
-from key import db_pass
-#  import pymysql
+import googlemaps
+from key import api_key, db_pass
 
 app = Flask(__name__, template_folder=".")
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyD8YhyiKR0S4FAC91f6L5bWDLROOh723pE"
+app.config['GOOGLEMAPS_KEY'] = api_key
 GoogleMaps(app)
+gmaps = googlemaps.Client(key=api_key)
 
 mysql = MySQL()
 
@@ -24,39 +25,41 @@ cursor = conn.cursor()
 
 @app.route("/")
 def mapview():
-    all_locations = []
+    all_symptom_locations = []
     cursor.execute("SELECT latitude, longitude, symptoms, disease FROM symptom")
     result = cursor.fetchall()
     for item in result:
-        location = {'lat' : item[0], 'lng' : item[1], 'infobox' : 'symptoms: ' + item[2] if item[3] is None else 'disease: ' + item[3] }
-        all_locations.append(location)
+        symptom = {'lat' : item[0], 'lng' : item[1], 'infobox' : 'symptoms: ' + item[2] if item[3] is None else 'disease: ' + item[3] }
+        all_symptom_locations.append(symptom)
+
+    all_hospitals = []
+    result = gmaps.places_nearby(location=(30.6123,-96.3413), radius=16094, type='hospital')
+    for hospital in result.get("results"):
+        lat = hospital.get("geometry").get("location").get("lat")
+        lng = hospital.get("geometry").get("location").get("lng")
+        place = {'lat' : lat, 'lng' : lng, 'infobox' : hospital.get("name") }
+        all_hospitals.append(place)
 
     # creating a map in the view
     campusmap = Map(
         identifier="campusmap",
      	lat= 30.6123, 
         lng= -96.3413,
-        markers = all_locations,
-        fit_markers_to_bounds = True
+        markers = all_symptom_locations,
+        fit_markers_to_bounds = True,
+        maptype_control = False,
+        streetview_control = False,
+        style = "height:400px;width:400px;margin:0;"
     )
     doctormap = Map(
-        identifier="doctormap",
-        lat= 30.6123, 
-        lng= -96.3413,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 30.6123, 
-             'lng': -96.3413,
-             'infobox': "<b>Where I am</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-            'lat': 30.6123, 
-             'lng': -96.3333,
-             'infobox': "<b>My Doctor</b>"
-          }
-        ]
+        identifier = "doctormap",
+        lat = 30.6123, 
+        lng = -96.3413,
+        markers = all_hospitals,
+        fit_markers_to_bounds = True,
+        maptype_control = False,
+        streetview_control = False,
+        style = "height:400px;width:400px;margin:0;"
     )
     return render_template('index.html', campusmap=campusmap, doctormap=doctormap)
 
